576c048aad5b4dee27d4c7d614e7d4a1e86f8676
AkashKumarYadav <akash.kumar.yadav.cse@gmail.com>
2020-04-06 19:30:32 +0530

    add parcel build process and extract pet component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-
-
-const Pet = ({ name, animal, breed }) => {
-
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ])
-}
-
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
     return React.createElement("div", { id: "something-important" }, [
         React.createElement("h1", {}, "Adopt ME"),
         React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "german" }),
-        React.createElement(Pet, { name: "Gatsby", animal: "Parrot", breed: "cocktail" }),
-        React.createElement(Pet, { name: "Tripy", animal: "cat", breed: "krean" })
-    ])
-}
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+        React.createElement(Pet, {
+            name: "Gatsby",
+            animal: "Parrot",
+            breed: "cocktail",
+        }),
+        React.createElement(Pet, { name: "Tripy", animal: "cat", breed: "krean" }),
+    ]);
+};
+render(React.createElement(App), document.getElementById("root"));
