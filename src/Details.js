b093c1af3f411c6f0f40649e07d0cea4445f1993
AkashKumarYadav <akash.kumar.yadav.cse@gmail.com>
2020-04-07 19:43:04 +0530

    added error boundries

--- src/Details.js
+++ src/Details.js
@@ -1,43 +1,49 @@
-import React from 'react';
-import pet from "@frontendmasters/pet"
-import Carousel from './Carousel';
+import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
+import ErrorBoundry from "./ErrorBoundry";
 
 class Details extends React.Component {
-    state = { loading: true }
-    componentDidMount() {
-        pet.animal(this.props.id).then(({ animal }) => {
-            this.setState({
-                name: animal.name,
-                animal: animal.type,
-                location: `${animal.contact.address.city}.${animal.contact.address.state}`,
-                description: animal.description,
-                breed: animal.breeds.primary,
-                media: animal.photos,
-                loading: false
-            })
-        })
+  state = { loading: true };
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}.${animal.contact.address.state}`,
+        description: animal.description,
+        breed: animal.breeds.primary,
+        media: animal.photos,
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading..</h2>;
     }
+    const { name, animal, location, description, breed, media } = this.state;
 
-
-    render() {
-        if (this.state.loading) {
-            return <h2>loading..</h2>;
-        }
-        const { name, animal, location, description, breed, media } = this.state;
-
-        return (
-            <div className="details">
-                <Carousel media={media} />
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{`${animal}-${breed}-${location}`}</h2>
-                    <button>Adopt {breed}</button>
-                    <p>{description}</p>
-                </div>
-
-            </div>);
-    }
-
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal}-${breed}-${location}`}</h2>
+          <button>Adopt {breed}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
+// export default Details;
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundry>
+      <Details {...props} />
+    </ErrorBoundry>
+  );
+}
---
+++ src/ErrorBoundry.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "@reach/router";
+
+class ErrorBoundry extends Component {
+  state = {
+    haserror: false,
+    redirect: false,
+  };
+
+  static getDerivedStateFromError() {
+    return { haserror: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundryCought an error", error, info);
+  }
+
+  componentDidUpdate() {
+    if (this.state.haserror) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.haserror) {
+      return (
+        <div>
+          <h1>
+            Something went wrong <Link to="/">click here</Link>for home
+          </h1>
+          <h2>or else wait </h2>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundry;
